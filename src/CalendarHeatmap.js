a092e9a1d7c64d45b8f8c2e790096e7a2a346f01
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-09 14:52:29 -0800

    Timeline Data Implementation

--- src/CalendarHeatmap.js
+++ src/CalendarHeatmap.js
@@ -2,6 +2,7 @@ import React, {useEffect, useRef, useState} from "react";
 import * as d3 from "d3";
 import {comparator, groupBy, msToTime} from "./helpers";
 import { updateBar, updateEmpty } from "./D3BarChart";
+import { updateTimeline, hideTimeline } from "./D3Timeline";
 
 const cellSize = 30;
 const cellMargin = 1;
@@ -24,6 +25,7 @@ const weeksInMonth = function(month){
 const CalendarHeatmap = (props) => {
     const streamingData = props.streamingData;
     const topArtistsData = props.topArtistsData;
+    const timelineData = props.timelineData;
     const calendarHeatmap = useRef();
 
     const MIN_MAX = d3.extent(streamingData.keys())
@@ -91,14 +93,19 @@ const CalendarHeatmap = (props) => {
                     d3.selectAll(".day").classed("selected", false)
                     d3.select(this).classed('hover', true);
                     d3.select(this).classed("selected", true);
-                    if (d.full_day_duration != 0)
-                        updateBar(d.artists)
-                    else
-                        updateEmpty()
+                    if (d.full_day_duration != 0) {
+                        updateBar(d.artists);
+                        updateTimeline(d.date, timelineData[d.date]);
+                    }
+                    else {
+                        updateEmpty();
+                        hideTimeline();
+                    }
                 } else {
                     d3.select(this).classed('hover', false);
                     d3.select(this).classed("selected", false)
                     updateBar(topArtistsData)
+                    hideTimeline();
                 }
             })
 
@@ -162,7 +169,7 @@ const CalendarHeatmap = (props) => {
             monthX.push(d.x + boxCentre + xOffset);
         })
 
-        let monthLabels = svg.append("g").attr("id","month-label")
+        let monthLabels = svg.append("g").attr("id", "month-label")
         const allMonths = [minDate, ...d3.timeMonths(minDate, maxDate)]
         allMonths.forEach(function(m,i)    {
             monthLabels.append("text")
---
+++ src/D3Timeline.js
@@ -0,0 +1,132 @@
+import React, {useRef, useEffect, useState} from "react";
+import * as d3 from 'd3';
+import { msToTime } from "./helpers"
+
+const margin = {
+    top: 40,
+    right: 10,
+    bottom: 10,
+    left: 10
+};
+
+const w = 2300 - margin.left - margin.right;
+const h = 250 - margin.top - margin.bottom;
+
+const barHeight = 100;
+
+const toolDate = d3.timeFormat("%d %b %Y");
+
+const extractYMD = function (s){
+    s = s.split("-")
+    return [parseInt(s[0]), parseInt(s[1]), parseInt(s[2])]
+}
+
+export function updateTimeline(date, dateData) {
+    const svg = d3.select("#timeline")
+        .select("svg")
+        .style("display", "flex");
+    svg.selectAll("g > *").remove();
+
+    svg.append("g")
+        .append("text")
+        .attr("class","date-title")
+        .attr("x", w / 2)
+        .attr("y", margin.top)
+        .text("Details for Date: " + toolDate(new Date(extractYMD(date))))
+
+    const chartStart = dateData[0].start
+    const chartEnd = dateData[dateData.length - 1].end
+
+    const xScale = d3.scaleLinear()
+        .domain([chartStart, chartEnd])
+        .range([margin.left, w - margin.right])
+
+    const xScaleTicks = d3.scaleTime()
+        .domain([new Date(chartStart), new Date(chartEnd)])
+        .range([margin.left, w - margin.right])
+
+    const xAxis = d3.axisBottom(xScaleTicks);
+
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${margin.top * 3 + barHeight - 20})`)
+        .call(xAxis);
+
+    let rect = svg.selectAll("rect")
+        .data(dateData)
+        .enter()
+        .append("g")
+
+    rect.append("rect")
+        .attr("class", "timeline-bar")
+        .attr("transform", `translate(0, ${margin.top * 2})`)
+
+    svg.selectAll("rect")
+        .data(dateData)
+        .transition()
+        .duration(1000)
+        .attr("x", function(d) {
+            return xScale(d.start)
+        })
+        .attr("y", margin.bottom)
+        .attr("width", function(d) {
+            return xScale(d.end) - xScale(d.start)
+        })
+        .attr("height", barHeight)
+
+
+    let div = d3.select("body").append("div").attr("class", "toolTip");
+    let timelineBars = svg.selectAll(".timeline-bar")
+
+    timelineBars.on("mousemove", function(event, d) {
+        d3.select(this).classed("hover", true);
+        div.style("left", event.pageX+10+"px");
+        div.style("top", event.pageY-25+"px");
+        div.style("display", "inline-block");
+        div.html((d.artist_name)+"<br>"+(msToTime(d.end - d.start)));
+    }).on("mouseout", function (d) {
+        d3.select(this).classed("hover", false)
+        div.style("display", "none");
+    }).on("click", function(event, d) {
+        if(!d3.select(this).classed("selected")){
+            d3.selectAll(".timeline-bar").classed("selected", false)
+            d3.select(this).classed("selected", true);
+        } else {
+            d3.select(this).classed("selected", false)
+        }
+    });
+
+}
+
+export function hideTimeline(){
+    d3.select("#timeline").select("svg").style("display", "none");
+}
+
+const D3Timeline = (props) => {
+
+    const timeline = useRef();
+    const timelineZoom = useRef();
+    const data = props.data;
+
+    useEffect(() => {
+        const svg = d3.select(timeline.current)
+            .attr("width", w + margin.left + margin.right)
+            .attr("height", h + margin.top + margin.bottom)
+            .style("margin", "40 10 10 10")
+            .style("display", "none")
+            // .attr("viewBox", "0 0 " + dimensions.width + " " + dimensions.height)
+    })
+
+    return (
+        <>
+            {/*<div id="timeline-zoom">*/}
+            {/*    <svg ref={timelineZoom}></svg>*/}
+            {/*</div>*/}
+            <div id="timeline">
+                <svg ref={timeline}></svg>
+            </div>
+        </>
+)
+}
+
+export default D3Timeline;
