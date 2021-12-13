8b8bc1242db8b9bab0fff7e94de6282c5ad87f11
jonatan <jonaengs@gmail.com>
2021-12-13 15:59:07 -0800

    add pipeline code from jonatan branch

---
+++ pipeline/jonatan_pipeline.py
@@ -0,0 +1,80 @@
+import json
+import pandas as pd
+import numpy as np
+import pprint
+
+from sklearn.preprocessing import StandardScaler
+
+from settings import data_path, write_final_df, write_dr_results, audio_features
+from jonatan_scrape import get_track_data, get_artist_data, get_audio_feature_data
+from jonatan_dr import compute_dr_results
+
+
+def read_streaming_history():
+    with open(data_path + "StreamingHistory0.json", mode="r", encoding="utf-8") as f:
+        data = json.loads(f.read())
+
+    df = pd.DataFrame(data)
+    df.endTime = pd.to_datetime(df.endTime)
+    return df
+
+def create_full_df(streamingHistory):
+    track_data = get_track_data(streamingHistory)
+    artist_data = get_artist_data(streamingHistory)
+    track_features = get_audio_feature_data(track_data)
+
+    # related_artists = pd.Series(scrape_related_artists(artist_data.artist_id), name="id")
+    
+    merged = pd.merge(streamingHistory, artist_data, left_on='artistName', right_on='artist_name', how='inner')
+    print(f"\tlost {streamingHistory.shape[0] - merged.shape[0]} entries when merging with artist_data")
+    print(streamingHistory[~streamingHistory.artistName.isin(merged.artistName)])
+    
+    merged = pd.merge(merged, track_data, left_on=["artistName", "trackName"], right_index=True, how="left")
+    
+    merged = pd.merge(merged, track_features, left_on="track_id", right_index=True, how="left")
+
+    if write_final_df:
+        keep_columns = list(streamingHistory.columns) \
+            + ["artist_genres", "artist_id", "artist_popularity", "track_duration_ms", "track_id", "track_popularity"]
+        write_df = merged[keep_columns]
+        json_str = write_df.to_json(orient="records")
+        with open(data_path + "merged_history.json", mode="w+", encoding="utf-8") as f:
+            f.write(json_str)
+
+    return merged
+
+def get_dr_results(merged):
+    for col in audio_features:
+        merged[col] = merged[col].transform(float)
+    merged[audio_features] = StandardScaler().fit_transform(merged[audio_features])  # Alternative: use MinMaxScaler to fit in specific range like [0, 1]
+
+    artist_data = get_artist_data(merged)
+
+    # drop entries where features are missing
+    nan_entries = merged.danceability.isna()
+    print(f"\tlost {nan_entries.sum()} entries when droppping entries missing features")
+    print(merged[nan_entries])
+    merged = merged[~nan_entries]
+
+    dr_results = compute_dr_results(merged, artist_data)
+
+    if write_dr_results:
+        dr_results = dr_results.replace([np.nan], [None])
+        json_str = dr_results.to_json(orient="records")
+        with open(data_path + "dr_results.json", mode="w+", encoding="utf-8") as f:
+            f.write(json_str)
+    
+    return dr_results
+
+def main():
+    print("Starting up pipeline...")
+    print("Reading streaming history...")
+    streamingHistory = read_streaming_history()
+    print("Constructing complete dataset...")
+    merged_df = create_full_df(streamingHistory)
+    print("Performing dimensionality reduction...")
+    dr_results = get_dr_results(merged_df)
+    print("COMPLETE!")
+
+if __name__ == '__main__':
+    main()
---
+++ pipeline/jonatan_scrape.py
@@ -0,0 +1,116 @@
+import json
+import pandas as pd
+import numpy as np
+
+from ast import literal_eval
+
+from settings import data_path, write_retrieved_data, retrieve_missing_tracks, retrieve_missing_artists, retrieve_missing_related_artists, retrieve_missing_track_audio_features
+from jonatan_requests import get_track_data_by_name, get_artist_data_by_name, get_related_artists, get_track_audio_features
+
+def get_artist_data(df):
+    artist_data = pd.DataFrame(_scrape_artist_data(df))
+    artist_data.columns = artist_data.columns.map(lambda s: "artist_" + s)
+    return artist_data
+
+def get_track_data(df):
+    track_data = pd.DataFrame(_scrape_track_data(df)).transpose()
+    track_data.columns = track_data.columns.map(lambda s: s if s.startswith("track_") else "track_" + s)
+    return track_data
+
+def get_audio_feature_data(merged_df):
+    track_features = pd.DataFrame(_scrape_track_audio_features(merged_df.track_id)).transpose()
+    return track_features
+
+def _scrape_track_data(df):
+    # Uniquely identify tracks by artist_name + track_name
+    # Since these are stored as a tuple, and json doesn't support tuple keys, extra work has to be done when reading and writing.
+    # Spotify has unique ids for all tracks, but our local data doesn't. TODO: Integrate track and artist id into streaming history
+    try:
+        with open(data_path + 'track_data.json', mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+            data = {literal_eval(k): v for k, v in data.items()}
+    except FileNotFoundError:
+        data = {}
+        
+    known_tracks = pd.Series(data.keys())
+    all_tracks = pd.Series(df[["artistName", "trackName"]].agg(tuple, axis=1).unique())
+    unknown_tracks = all_tracks[~all_tracks.isin(known_tracks)]
+    
+    if retrieve_missing_tracks:
+        try:
+            for artist, track in unknown_tracks:
+                result = get_track_data_by_name(artist, track)
+                if result:
+                    data[(artist, track)] = result
+        finally:
+            if write_retrieved_data:
+                with open(data_path + 'track_data.json', mode='w+', encoding='utf-8') as f:
+                    json.dump({str(k): v for k,v in data.items()}, f)
+    return data
+
+def _scrape_artist_data(df):
+    try:
+        with open(data_path + 'artist_data.json', mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = []
+        
+    known_artists = pd.Series([artist['name'] for artist in data if artist is not None])
+    all_artists = pd.Series(df.artistName.unique())
+    unknown_artists = all_artists[~all_artists.isin(known_artists)]
+    
+    if retrieve_missing_artists:
+        try:
+            for artist in unknown_artists:
+                result = get_artist_data_by_name(artist)
+                if result:
+                    data.append(result)
+        finally:
+            if write_retrieved_data:
+                with open(data_path + 'artist_data.json', mode='w+', encoding='utf-8') as f:
+                    json.dump(data, f)
+    return data
+
+
+def _scrape_related_artists(artist_ids):
+    try:
+        with open(data_path + 'related_artist_data.json', mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+    
+    known_related = pd.Series(data.keys())
+    missing_related = artist_ids[~artist_ids.isin(known_related)]
+    
+    if retrieve_missing_related_artists:
+        try:
+            for artist_id in missing_related:
+                if (result := get_related_artists(artist_id)):
+                    data[artist_id] = result
+        finally:
+            if write_retrieved_data:
+                with open(data_path + 'related_artist_data.json', mode='w+', encoding='utf-8') as f:
+                    json.dump(data, f)
+    return data
+
+def _scrape_track_audio_features(track_ids):
+    try:
+        with open(data_path + 'track_feature_data.json', mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+
+    known_features = set(data.keys())
+    missing_features = set(tid for tid in set(track_ids) if not tid in known_features)
+        
+    if retrieve_missing_track_audio_features:
+        try:
+            for track_id in missing_features:
+                if (result := get_track_audio_features(track_id)):
+                    data[track_id] = result
+        finally:
+            if write_retrieved_data:
+                with open(data_path + 'track_feature_data.json', mode='w+', encoding='utf-8') as f:
+                    json.dump(data, f)
+
+    return data
